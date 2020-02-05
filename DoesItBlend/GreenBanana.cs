37a0cb2232a90b96bbbae910ecef9a975921a237
erj2016 <erj2016@ksu.edu>
2020-02-05 16:33:10 -0600

    Will it Blend Exercise

---
+++ DoesItBlend/GreenBanana.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    public class GreenBanana : Banana
+    {
+        /// <summary>
+        /// See how we can't use override b/c banana blend is sealed, we must use new to create this method.
+        /// </summary>
+        /// <returns></returns>
+        public new string Blend()
+        {
+            return "lumps";
+        }
+    }
+}
